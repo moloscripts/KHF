2012186db9a90e815c52b7e18341978ce7d924b5
Molo Muli <molo.andrew@gmail.com>
2021-04-05 11:33:20 +0300

    Univariate Descriptive Analysis

--- Scripts/HF-Analyisis.R
+++ Scripts/HF-Analyisis.R
@@ -29,7 +29,7 @@ CountyBedCapacity$Observation <- 1:nrow(CountyBedCapacity)
 BedCapacity <- merge(CountyBedCapacity, KenyaCountyPop, by ="Observation")
 BedCapacity <- BedCapacity %>%
   select(County.y, Male, Female, Intersex, Total, `Bed Capacity`) %>%
-  rename(County = County.y) %>%
+  rename(County = County.y)
   
 # Bed Capacity Per 1000 Population
 BedCapacity <- BedCapacity %>%
@@ -38,7 +38,7 @@ BedCapacity <- BedCapacity %>%
 # Visual of Bed Capacity per 1000 Pop
 ggplot(BedCapacity, aes(reorder(County,`Bed Capacity Per 1000 Population`), `Bed Capacity Per 1000 Population`)) +
   geom_col(fill="#f48b29") +
-  labs(x="County") +
+  labs(x="") +
   geom_text(aes(label=`Bed Capacity Per 1000 Population`), vjust=1, size=3) +
   theme_minimal() +
   coord_flip()
@@ -69,3 +69,47 @@ FinalKenyanHospitals <- KenyanHospitals %>%
 
 kurtosis(FinalKenyanHospitals$`Bed Capacity`)
 skim(FinalKenyanHospitals)
+
+
+## Univariate Analysis ####
+
+### Distribution of Bed Capacity ####
+ggplot(FinalKenyanHospitals, aes(`Bed Capacity`)) + 
+  geom_histogram(aes( y = ..density..), color="red", fill="#9ede73") + 
+  stat_function(fun = dnorm, args = list(mean = mean(FinalKenyanHospitals$`Bed Capacity`),
+                                         sd = sd(FinalKenyanHospitals$`Bed Capacity`))) + 
+  theme_minimal()
+
+qqnorm(FinalKenyanHospitals$`Bed Capacity`)
+qqline(FinalKenyanHospitals$`Bed Capacity`)
+
+
+### DF Count of Health Facility Types Country Wide ####
+HF_Count <- KenyanHospitals %>%
+  group_by(`Facility Type`) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count)) %>%
+  mutate(percent = round(count/sum(count) * 100,1))
+class(HF_Count)
+
+#### Visual
+KenyanHospitals %>%
+  group_by(`Facility Type`) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count)) %>%
+  mutate(percent = round(count/sum(count) * 100,1)) %>%
+  ggplot(aes(x=reorder(`Facility Type`, count), y=count)) + 
+  geom_col(fill="#ffe268") +
+  labs(x="") +
+  coord_flip() + 
+  theme_minimal()
+
+
+### DF Count of Various Levels 
+HF_levels <- KenyanHospitals %>%
+  group_by(Level) %>%
+  summarise(count = n()) %>%
+  arrange(desc(count)) %>%
+  mutate(`Percentage (%)` = round(count/sum(count) * 100,1))
+
+
